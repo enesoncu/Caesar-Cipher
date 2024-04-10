b8601c0268c56cc4e681b63fb3d631855f9c716b
Enes Öncü <enesoncuu@hotmail.com>
2024-04-10 23:24:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from art import logo
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount) % 26
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+print(logo)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart.lower() != "yes":
+        should_end = True
+        print("Goodbye")
